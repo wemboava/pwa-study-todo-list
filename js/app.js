760afb3d751355694225133820062ca25bb0f016
Willian <willian@quicktech.no>
2018-12-05 08:11:58 -0200

    bug fix service worker

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ import { Controller } from './toDoController';
 import { ToDoNotification } from './toDoNotification';
 
 if('serviceWorker' in navigator){
-    navigator.serviceWorker.register('./service_worker.js')
+    navigator.serviceWorker.register('../dist/service_worker.js')
         .then((registration) => {
             console.log('Service Worker registered with scope: ', registration.scope);
             ToDoNotification.subscribeUserToNotification();
